b5f348c72567adc71e33109f760741632d9265cb
samkiyya <samuelkiyya2012@gmail.com>
2026-03-17 16:34:16 +0300

    Internationalization using the accept-language header

--- spring-boot/rest-web-service/src/main/java/dev/samuelaberra/rest_service/helloworld/HelloWorld.java
+++ spring-boot/rest-web-service/src/main/java/dev/samuelaberra/rest_service/helloworld/HelloWorld.java
@@ -3,6 +3,11 @@ package dev.samuelaberra.rest_service.helloworld;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,6 +17,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloWorld {
+	private MessageSource messageSource;
+	public HelloWorld(MessageSource messageSource) {
+		this.messageSource=messageSource;
+	}
 //	@RequestMapping(method = RequestMethod.GET, path="/hello-world")
 
 	// a bit longer but readable form is @GetMapping(path="/hello-world")
@@ -32,5 +41,15 @@ public class HelloWorld {
 		
 		return new HelloWorldBean(String.format("Hello %S", name));
 	}
+	
+	@GetMapping("/hello-world-internationalized") 
+	public String helloWorldInternationalized() {
+		//1: good.mornning.message
+		//2: 
+		// message code, any variable to be replaced as template, default message, local/ language to be picked
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.mornning.message", null, "default message", locale);
+	}
+
 }
 
